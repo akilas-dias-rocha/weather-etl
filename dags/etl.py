622e33eec9e7db9f0a667f50ca5fcd9b4175c2a3
Akilas Dias <akilas.rocha@indicium.tech>
2026-07-01 14:38:17 -0300

    feat: fixing tasks decorators

--- dags/etl.py
+++ dags/etl.py
@@ -3,23 +3,20 @@ from airflow.models import Variable
 from pendulum import datetime
 
 import requests
-import logging
 import os
 import pandas as pd
 
 
-logger = logging.getLogger(__name__)
-
 @dag(
     start_date=datetime(2026, 1, 1),
     schedule="0 6 * * *",
-    default_args={"owner": "Astro", "retries": 3},
+    default_args={"owner": "Astro", "retries": 2},
     tags=["Pernambuco"],
     catchup=False,
 )
 def weather_etl():
 
-    @task.python
+    @task(retries=3, retry_delay=10)
     def extract_weather_data():
         """
         Extracts weather data from the WeatherAPI for a specific location
@@ -40,20 +37,20 @@ def weather_etl():
         }
         r = requests.get(complete_url, params=params, timeout=10)
 
-        if r.status_code == 200:
-            return r.json()
-        else:
-            logger.error(f"Error fetching weather data: {r.status_code}")
+        if r.status_code != 200:
+            print(f"Error fetching weather data: {r.status_code}")
             return None
+        else:
+            return r.json()
 
-    @task.python
+    @task()
     def transform_weather_data(weather_data: dict = None):
         """
         Transforms the weather data into a pandas DataFrame.
         """
 
         if weather_data is None:
-            logger.warning("No data to transform.")
+            print("No data to transform.")
             return []
 
         location = weather_data.get("location", {})
@@ -76,7 +73,7 @@ def weather_etl():
         }
         return [transformed_data]
 
-    @task.python
+    @task()
     def load_weather_data(data: list = None, **context):
         """
         Loads the transformed weather data into a CSV file.
@@ -93,15 +90,15 @@ def weather_etl():
             file_name = f"weather_data_{file_date}.csv"
             output_path = os.path.join(output_dir, file_name)
             df.to_csv(output_path, index=False)
-            logger.info(f"Weather data loaded successfully to {output_path}")
+            print(f"Weather data loaded successfully to {output_path}")
         except Exception as e:
-            logger.error(f"Error loading weather data: {e}")
+            print(f"Error loading weather data: {e}")
             raise
 
     # Define task dependencies with data passing
     extract_output = extract_weather_data()
     transform_output = transform_weather_data(extract_output)
-    load_weather_data(transform_output)
+    load_all_data = load_weather_data(transform_output)
 
 # Instantiate the DAG
 weather_etl()
