32b4df3fa33783be3e2b6184d655f8f980589f7d
Akilas Dias <akilas.rocha@indicium.tech>
2026-07-02 22:42:25 -0300

    feat: create weather_data table schema

---
+++ dags/sql/weather_table_ddl.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS weather_data (
+    name VARCHAR(100),
+    region VARCHAR(100),
+    country VARCHAR(100),
+    tz_id VARCHAR(100),
+    localtime TIMESTAMP,
+    date DATE,
+    last_updated TIMESTAMP,
+    condition VARCHAR(100),
+    maxtemp_c FLOAT,
+    mintemp_c FLOAT,
+    avgtemp_c FLOAT,
+    chance_of_rain FLOAT
+);
