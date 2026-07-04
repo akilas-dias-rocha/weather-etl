a08b03c0fcb1a612975b64a26d77003f8cb4560d
Akilas Dias <akilas.rocha@indicium.tech>
2026-07-03 22:50:09 -0300

    fix: add missing quotes to column names in weather_data table schema

--- dags/sql/weather_table_ddl.sql
+++ dags/sql/weather_table_ddl.sql
@@ -3,12 +3,13 @@ CREATE TABLE IF NOT EXISTS weather_data (
     region VARCHAR(100),
     country VARCHAR(100),
     tz_id VARCHAR(100),
-    localtime TIMESTAMP,
-    date DATE,
+    "localtime" TIMESTAMP,
+    "date" DATE,
     last_updated TIMESTAMP,
-    condition VARCHAR(100),
+    "condition" VARCHAR(100),
     maxtemp_c FLOAT,
     mintemp_c FLOAT,
     avgtemp_c FLOAT,
-    chance_of_rain FLOAT
+    chance_of_rain FLOAT,
+    UNIQUE (name, "date")
 );
